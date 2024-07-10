34f96fbda0c2cfe717c95863d8fdd248fb39684f
Somyasri1 <153505085+Somyasri1@users.noreply.github.com>
2024-07-10 12:01:21 +0530

    Create greatest of two numbers.java

---
+++ greatest of two numbers.java
@@ -0,0 +1,16 @@
+public class great 
+{ 	
+	public static void main(String[] args)
+	{
+		int x=20,y=50;
+		if(x==y)
+			System.out.println("both are equal");
+		else if (x>y)
+			System.out.println("is greater" + x);
+		else
+		 	System.out.println("is greatest" + y);
+	}
+}
+
+out put:
+is greatest50
