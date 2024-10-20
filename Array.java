5c39033a29ed3ea409c8c31e80dc80892ac513c8
Somyasri1 <153505085+Somyasri1@users.noreply.github.com>
2024-10-20 20:22:32 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -5,4 +5,34 @@ public class Array{
           System.out.println("element at index" + ":"+ numbers[i]);
        }
     }
- 
+
+
+1)write a java program to display default value of all primitive data type of java.
+      
+  //i/p 
+    class Default {
+    static int i;
+    static double d;
+    static float f;
+    static char c;
+    static boolean b;
+    static long l;
+    public static void main(String[] args){
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(l);
+    }
+}
+     //o/p
+      0
+0.0
+0.0
+.
+false
+0
+
+2)
+      
